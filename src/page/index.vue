f57b0f2395dc318c3caef3d54a197758c16741af
siwenyu <siwenyu@baidu.com>
2018-05-15 20:51:03 +0800

    s

---
+++ src/page/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        请开始你的表演
+    </div>
+</template>
+
+<script>
+import tool from  '@/assets/js/tool.js'
+import dataFile from  '@/assets/js/dataFile.js'
+
+
+export default {
+    name: 'index',
+    components: {
+        
+    },
+    data () {
+        return {
+            
+        }
+    },
+    methods:{
+        
+    },
+    mounted(){
+        
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
